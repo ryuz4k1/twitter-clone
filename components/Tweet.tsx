54899c2ea1b86a1e2dd0ed1becdc63912e9bb99e
ryuz4k1 <ozerr.murat@hotmail.com>
2022-10-30 15:53:17 +0300

    tweet component edited

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Tweet } from "../typings";
 import TimeAgo from "react-timeago";
+import {
+  ChatAlt2Icon,
+  HeartIcon,
+  SwitchHorizontalIcon,
+  UploadIcon,
+} from "@heroicons/react/outline";
 
 interface Props {
   tweet: Tweet;
@@ -30,7 +36,29 @@ function Tweet({ tweet }: Props) {
             />
           </div>
           <p className="pt-1"> {tweet.text} </p>
-          {tweet.image && <img className="mr-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm" src={tweet.image} alt="" /> }
+          {tweet.image && (
+            <img
+              className="mr-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
+              src={tweet.image}
+              alt=""
+            />
+          )}
+        </div>
+      </div>
+
+      <div className="mt-5 flex justify-between">
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <ChatAlt2Icon className="h-5 w-5"/>
+          <p>5</p>
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <SwitchHorizontalIcon className="h-5 w-5"/>
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <HeartIcon className="h-5 w-5"/>
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <UploadIcon className="h-5 w-5"/>
         </div>
       </div>
     </div>
